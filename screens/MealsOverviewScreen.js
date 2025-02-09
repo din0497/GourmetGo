4010658fc2683479c05919f6a9ebd66a004ae993
Din <rivojidin97@gmail.com>
2025-02-09 23:09:52 +0900

    fix: render list of Favs

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,48 +1,26 @@
-import { View, FlatList, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
 
-import { CATEGORIES, MEALS } from "../data/dummy-data";
-import MealItem from "../components/MealItem";
-import { useEffect, useLayoutEffect } from "react";
+import MealsList from "../components/MealsList/MealsList";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 
-export default function MealsOverviewScreen({ route, navigation }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
   useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
     navigation.setOptions({
-      title: CATEGORIES.find((category) => category.id === catId).title,
+      title: categoryTitle,
     });
   }, [catId, navigation]);
 
-  function renderMealItem(itemData) {
-    return (
-      <MealItem
-        id={itemData.item.id}
-        title={itemData.item.title}
-        imageUrl={itemData.item.imageUrl}
-        duration={itemData.item.duration}
-        complexity={itemData.item.complexity}
-        affordability={itemData.item.affordability}
-      />
-    );
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
+  return <MealsList items={displayedMeals} />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
+export default MealsOverviewScreen;
